5b018bde1fc59cdd5dfec27f6e4cb6dafe2b5109
Amy Vincent <106971274+Blackadder1712@users.noreply.github.com>
2023-09-03 19:10:40 +0100

    Animations
    adding idle, move and attack animations to enemy

--- Assets/Scripts/weapon.cs
+++ Assets/Scripts/weapon.cs
@@ -8,6 +8,7 @@ public class weapon : MonoBehaviour
     [SerializeField] float range = 100f;//length of ray
     [SerializeField] float damage = 30f; // gun strength
     [SerializeField] ParticleSystem muzzleflash;
+    [SerializeField] GameObject hitEffect;
 
     void Update()
     {
@@ -34,16 +35,22 @@ public class weapon : MonoBehaviour
             RaycastHit hit;
             if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))//detection ray location, shoot forward from camera
             {
-            Debug.Log("I hit:" + hit.transform.name);
-            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();//access health element
-            if(target == null) return;
-            target.TakeDamage(damage); //apply gun damage to health
+                CreateHitImpact(hit); 
+                EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();//access health element
+                if(target == null) return;
+                target.TakeDamage(damage); //apply gun damage to health
             }
             else //if dont hit anything 
             {
                 return;
             }
         
+        }
+
+        private void CreateHitImpact(RaycastHit hit) //check where bullet hit 
+        {
+           GameObject impact =Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal)); // explosion right way
+           Destroy(impact, 1); //after one second;
         }
     
 }
